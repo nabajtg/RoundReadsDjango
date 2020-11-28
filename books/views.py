b17545fc4fb0c70857a38a12dffc9961a32583dc
Naba Jyoti <nabajtg@gmail.com>
2020-11-28 18:52:54 +0530

    Search functionality added

--- books/views.py
+++ books/views.py
@@ -5,7 +5,7 @@ from rest_framework import filters
 from rest_framework.pagination import PageNumberPagination
 
 class SetPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 12
     #page_size_query_param = 'page_size'
     #max_page_size = 1000
 
@@ -14,8 +14,8 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     pagination_class = SetPagination
-    #search_fields = ['available_for', 'title', 'description', 'author']
-    #filter_backends = (filters.SearchFilter,)
+    search_fields = ['title', 'desc', 'author']
+    filter_backends = (filters.SearchFilter,)
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
