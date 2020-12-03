e58c247fb64ceb1738a16fa837a222487752217b
Naba Jyoti Gogoi <nabajtg@gmail.com>
2020-12-03 13:58:17 +0530

    User model changed to Store other info

--- authentications/models.py
+++ authentications/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, fname, mname, lname, password=None):
+    def create_user(self, email, fname, mname, lname, phone, address, yearOfEnrollment, yearOfGraduation, dept, roll, hostel, password=None):
         if not email:
             raise ValueError('Users must have an email address')
         
         email = self.normalize_email(email)
-        user = self.model(email=email, fname=fname, mname=mname, lname=lname)
+        user = self.model(email=email, fname=fname, mname=mname, lname=lname, phone=phone, address=address, yearOfEnrollment=yearOfEnrollment, yearOfGraduation=yearOfGraduation, dept=dept, roll=roll, hostel=hostel)
 
         user.set_password(password)
         user.save()
@@ -20,14 +20,21 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True, primary_key=True)
     fname = models.CharField(max_length=255)
     mname = models.CharField(max_length=255, blank=True)
-    lname = models.CharField(max_length=255)    
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    lname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    yearOfEnrollment = models.CharField(max_length=4, blank=True)
+    yearOfGraduation = models.CharField(max_length=4, blank=True)
+    dept = models.CharField(max_length=255, blank=True)
+    roll = models.CharField(max_length=10, blank=True)
+    hostel = models.CharField(max_length=255, blank=True)
+    is_active = models.BooleanField(default=True, blank=True)
+    is_staff = models.BooleanField(default=False, blank=True)
 
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['fname', 'mname', 'lname']
+    REQUIRED_FIELDS = ['fname', 'mname', 'lname', 'phone', 'address', 'yearOfEnrollment', 'yearOfGraduation', 'dept', 'roll', 'hostel']
 
     
     def __str__(self):
