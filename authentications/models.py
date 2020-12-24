d29621009c7a5208b36e95e50d9c182a0111d9d4
Naba Jyoti Gogoi <nabajtg@gmail.com>
2020-12-24 10:24:34 +0530

    wishlist and liked_blogs added to auth model

--- authentications/models.py
+++ authentications/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, fname, mname, lname, phone, address, yearOfEnrollment, yearOfGraduation, dept, roll, hostel, password=None):
+    def create_user(self, email, fname, mname, lname, phone, address, yearOfEnrollment, yearOfGraduation, dept, roll, hostel, wishlist, liked_blogs, password=None):
         if not email:
             raise ValueError('Users must have an email address')
         
         email = self.normalize_email(email)
-        user = self.model(email=email, fname=fname, mname=mname, lname=lname, phone=phone, address=address, yearOfEnrollment=yearOfEnrollment, yearOfGraduation=yearOfGraduation, dept=dept, roll=roll, hostel=hostel)
+        user = self.model(email=email, fname=fname, mname=mname, lname=lname, phone=phone, address=address, yearOfEnrollment=yearOfEnrollment, yearOfGraduation=yearOfGraduation, dept=dept, roll=roll, hostel=hostel, wishlist=wishlist, liked_blogs=liked_blogs)
 
         user.set_password(password)
         user.save()
@@ -30,11 +30,13 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     hostel = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True, blank=True)
     is_staff = models.BooleanField(default=False, blank=True)
+    wishlist = models.JSONField(blank=True, default=dict)
+    liked_blogs = models.JSONField(blank=True, default=dict)
 
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['fname', 'mname', 'lname', 'phone', 'address', 'yearOfEnrollment', 'yearOfGraduation', 'dept', 'roll', 'hostel']
+    REQUIRED_FIELDS = ['fname', 'mname', 'lname', 'phone', 'address', 'yearOfEnrollment', 'yearOfGraduation', 'dept', 'roll', 'hostel', 'wishlist', 'liked_blogs']
 
     
     def __str__(self):
